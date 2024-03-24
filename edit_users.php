07adb260b36eca6c298d9a3cf91e48fb398bcf10
Montel-t <montelthondu@gmail.com>
2024-03-24 21:39:00 +0300

    Changes to system pages & functions

--- edit_users.php
+++ edit_users.php
@@ -1,26 +1,38 @@
 <?php include("templates/header.php"); ?>
 <body>
 <?php include("templates/nav.php"); ?>
+<?php 
+
+require_once "includes/dbConnect.php";
+
+$userId = $_GET["userId"];
+
+$get_user = "SELECT * FROM `users` LEFT JOIN `genders` USING (`genderId`) WHERE userId=$userId LIMIT 1";
+
+$get_user_res = $dbConn->query($get_user);
+$get_user_row = $get_user_res->fetch_assoc();
+
+?>
 <div class="header">
     <h1>Header</h1>
 </div>
 <div class="row">
     <div class="content">
-        <h3>Sign Up</h3>
-<form action="processes/sign_up.php" method="post">
+        <h3>Edit User</h3>
+<form action="processes/edit_user.php" method="post">
     <label for="fullname">Full Name: </label><br>
-    <input type="text" id="fullname" name="fullname" placeholder="Full Name" autofocus required /><br>
+    <input type="text" id="fullname" name="fullname" placeholder="Full Name" autofocus required value="<?php print $get_user_row["fullname"]; ?>"/><br>
 
     <label for="email_address">Email Address: </label><br>
-    <input type="email" id="email_address" name="email_address" placeholder="Email Address" /><br>
+    <input type="email" id="email_address" name="email_address" placeholder="Email Address" value="<?php print $get_user_row["email"]; ?>" /><br>
     
     <label for="password">Password: </label><br>
     <input type="password" id="password" name="password" placeholder="Password" /><br>
     <label for="gender">Gender : </label><br>
     <select id="gender" required name="gender">
+        <option value="<?php print $get_user_row["genderId"]; ?>"><?php print $get_user_row["gender"]; ?></option>
         <option value="">--Select Gender--</option>
 <?php
-require_once "includes/dbConnect.php";
 
 $select_gender = "SELECT * FROM genders ORDER BY genderId ASC";
 
@@ -35,16 +47,15 @@ print "<option value='".$gender_row["genderId"]."'>".$gender_row["gender"]."</op
     </select>
     <br>
     <label for="Address"> Address : </label><br>
-<textarea id="Address" name="Address" placeholder="Enter your Address here" col="30" rows="5"></textarea>
+<textarea id="Address" name="Address" placeholder="Enter your Address here" col="30" rows="5"><?php print $get_user_row["address"]; ?></textarea>
     <br>
-    <input type="submit" name="" value="Save Details" />
+    <input type="submit" name="" value="Update Details" />
+    <input type="hidden" name="userId" value="<?php print $get_user_row["userId"]; ?>" />
     <br>
-
 </form>
     </div>
     <div class="side_bar">
         <h3>Sign In</h3>
-        
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     </div>
 </div>
--- templates/circle.php
+++ templates/circle.php
@@ -5,16 +5,17 @@
 <nav role="navigation" class="primary-navigation">
   <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="test.php">Products &dtrif;</a>
+    <li><a href="products.php">Products &dtrif;</a>
       <ul class="dropdown">
-        <li><a href="#">Men's</a></li>
-        <li><a href="#">Women's</a></li>
-        <li><a href="#">Sports</a></li>
-        <li><a href="#">Luxury</a></li>
+        <li><a href="mens.php">Men's</a></li>
+        <li><a href="womens.php">Women's</a></li>
+        <li><a href="sports.php">Sports</a></li>
+        <li><a href="luxury.php">Luxury</a></li>
         <!-- <img src="images/lights/images.jpg"/> -->
       </ul>
     </li>
     <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
     <li><a href="#">Contact</a></li>
   </ul>
 </nav>
